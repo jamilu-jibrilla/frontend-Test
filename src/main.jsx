332f4e107f82a95fe96c5c20bf56c6bf4f72e314
jamil <jamilujibrilla01@gmail.com>
2022-07-03 03:14:06 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Render from './Render'
-import './style.css'
+import './styles/App.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
